37c1006f150c8cfca1218d898ece56a4f3c529b2
Henrique Artur <contato@henriqueartur.com>
2023-07-08 14:29:40 -0300

    :wrench: Add mongo config prod & dev

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -3,11 +3,24 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://root:troqueASenha@localhost:27017', {
-      dbName: 'likeThisMovie',
+    ConfigModule.forRoot({
+      envFilePath: [process.env.NODE_ENV == 'PROD' ? '.prod.env' : '.dev.env'],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          uri: configService.get<string>(`MONGO_CONNECTION`),
+          ...(process.env.NODE_ENV == 'PROD'
+            ? { dbName: configService.get<string>(`MONGO_DB_NAME`) }
+            : {}),
+        };
+      },
+      inject: [ConfigService],
     }),
     UsersModule,
   ],
