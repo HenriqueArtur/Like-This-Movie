e2de45576c3e166932235f90661f974518f62f9c
Henrique Artur <contato@henriqueartur.com>
2023-07-08 19:31:03 -0300

    :sparkles: Auth services

--- packages/server/src/auth/auth.service.ts
+++ packages/server/src/auth/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { UserMongoService } from 'src/users/services/mongo-user.service';
+import { sign } from 'jsonwebtoken';
+import { PayloadDto } from './dto/payload.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserMongoService) {}
+
+  async signPayload(payload: PayloadDto) {
+    return sign(payload, process.env.JWT_SECRET, { expiresIn: '7d' });
+  }
+
+  async validateUser(payload: PayloadDto) {
+    return await this.userService.findByPayload(payload);
+  }
+}
