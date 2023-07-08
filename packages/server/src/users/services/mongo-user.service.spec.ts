30207862c926a074f7eaa4bf29fa4f3bee067510
Henrique Artur <contato@henriqueartur.com>
2023-07-08 12:28:09 -0300

    :white_check_mark: Mongo user service test

---
+++ packages/server/src/users/services/mongo-user.service.spec.ts
@@ -0,0 +1,51 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { UserMongoService } from './mongo-user.service';
+import { faker } from '@faker-js/faker';
+
+describe('UserMongoService', () => {
+  let service: UserMongoService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UserMongoService,
+        {
+          provide: getModelToken('User'),
+          useValue: Model,
+        },
+      ],
+    }).compile();
+
+    service = module.get<UserMongoService>(UserMongoService);
+  });
+
+  describe('create', () => {
+    it('should create a new user', async () => {
+      const id = faker.database.mongodbObjectId();
+      const login = faker.internet.email();
+      const password = faker.internet.password();
+      const doc: CreateUserDto = {
+        login,
+        password,
+      };
+
+      const saveSpy = jest.spyOn(service, 'create').mockResolvedValueOnce({
+        id,
+        login,
+        password,
+      });
+
+      const result = await service.create(doc);
+
+      expect(saveSpy).toHaveBeenCalledTimes(1);
+      expect(result).toEqual({
+        id,
+        login,
+        password,
+      });
+    });
+  });
+});
