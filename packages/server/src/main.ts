cfd86bb3b8b1b22d76f38cbdc52d0cbcbf460e15
Henrique Artur <contato@henriqueartur.com>
2023-07-09 19:14:29 -0300

    :sparkles: Add logo on init server

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -3,6 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = 3000;
+  await app.listen(port);
+  console.log(`
+  _      _ _          _______ _     _       __  __            _      
+ | |    (_) |        |__   __| |   (_)     |  \\\/  |          (_)     
+ | |     _| | _____     | |  | |__  _ ___  | \\  / | _____   ___  ___ 
+ | |    | | |/ / _ \\    | |  | '_ \\| / __| | |\\/| |/ _ \\ \\ / / |/ _ \\
+ | |____| |   <  __/    | |  | | | | \\__ \\ | |  | | (_) \\ V /| |  __/
+ |______|_|_|\\_\\___|    |_|  |_| |_|_|___/ |_|  |_|\\___/ \\_/ |_|\\___| 
+
+ By: Henrique Artur <contato@henriqueartur.com>
+ Description: A test for Profectum + Unimed
+  `);
+  console.log(` Server running on port: ${port}`);
 }
 bootstrap();
