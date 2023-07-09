7f003833a09c5ade8494914298e941e4754b09e9
Henrique Artur <contato@henriqueartur.com>
2023-07-09 09:04:39 -0300

    :sparkles: Begin movies module

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
   imports: [
@@ -25,6 +26,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     UsersModule,
     AuthModule,
+    MoviesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
