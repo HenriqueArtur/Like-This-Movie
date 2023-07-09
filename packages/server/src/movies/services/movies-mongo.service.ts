f9c811a6198b277e578901f2c2e1942ed4e207a6
Henrique Artur <contato@henriqueartur.com>
2023-07-09 14:47:07 -0300

    :sparkles: FindOrCreateByTmdbIds & updateLikes movie mongo

---
+++ packages/server/src/movies/services/movies-mongo.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { MovieModel } from '../models/movie.model';
+import { Movie } from '../dto/movie.dto';
+
+@Injectable()
+export class MoviesMongoService {
+  constructor(
+    @InjectModel('Movie') private readonly userModel: Model<MovieModel>,
+  ) {}
+
+  async findOrCreateByTmdbIds(tmdbIds: number[]): Promise<Movie[]> {
+    const existingMovies = await this.userModel.find({
+      tmdb_id: { $in: tmdbIds },
+    });
+    const existingTmdbIds = existingMovies.map((movie) => movie.tmdb_id);
+    const missingTmdbIdsPromise = tmdbIds
+      .filter((tmdbId) => !existingTmdbIds.includes(tmdbId))
+      .map((tmdb_id) =>
+        new this.userModel({
+          tmdb_id,
+          likes: 0,
+        }).save(),
+      );
+    const newMovies = await Promise.all(missingTmdbIdsPromise);
+    return [...existingMovies, ...newMovies].map((movieMongo) => ({
+      id: movieMongo.id,
+      tmdb_id: movieMongo.tmdb_id,
+      likes: movieMongo.likes,
+    }));
+  }
+
+  async updateLikes(
+    id: string,
+    operation: 'SUM' | 'SUBTRACT' = 'SUM',
+  ): Promise<Movie> {
+    const movieDoc = await this.userModel.findById(id);
+    movieDoc.likes = movieDoc.likes + (operation == 'SUM' ? 1 : -1);
+    await movieDoc.save();
+    return {
+      id: movieDoc.id,
+      likes: movieDoc.likes,
+      tmdb_id: movieDoc.tmdb_id,
+    };
+  }
+}
