9c15282a16ab9fca27756af71c704e3ae2013e0b
Henrique Artur <contato@henriqueartur.com>
2023-07-09 19:16:28 -0300

    :fire: Delete app empty files

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { MoviesModule } from './movies/movies.module';
@@ -28,7 +26,7 @@ import { MoviesModule } from './movies/movies.module';
     AuthModule,
     MoviesModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
