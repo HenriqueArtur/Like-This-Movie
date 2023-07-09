277356a19e5f181c8471bfa54f4eec24d7dc23e1
Henrique Artur <contato@henriqueartur.com>
2023-07-09 17:52:56 -0300

    :sparkles: Toggle like movie

--- packages/server/src/movies/movies.controller.ts
+++ packages/server/src/movies/movies.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UseGuards,
+  Response,
+  Request,
+  Post,
+  Param,
+  HttpStatus,
+} from '@nestjs/common';
 import { TmdbApiService } from './services/tmdb-api.service';
 import { TmdbDomainService } from './services/tmdb-domain.service';
 import { MoviesMongoService } from './services/movies-mongo.service';
 import { AuthGuard } from '@nestjs/passport';
 import { UsersLikesMongoService } from './services/users-likes-mongo.service';
+import { Response as Res } from 'express';
 
 @Controller('movies')
 export class MoviesController {
@@ -37,5 +47,24 @@ export class MoviesController {
         userLiked: !!userLikesIds.includes(movieInApp.tmdb_id),
       };
     });
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post('/toggle-like/:tmdb_id')
+  async toggleLike(
+    @Request() req: any,
+    @Response() res: Res,
+    @Param('tmdb_id') tmdb_id: number,
+  ) {
+    const userId = req.user.id;
+    const like = await this.usersLikesService.findByTmdbId(tmdb_id);
+    if (!like) {
+      const newLike = await this.usersLikesService.create(userId, tmdb_id);
+      await this.moviesService.updateLikes(tmdb_id);
+      return res.status(HttpStatus.CREATED).send(newLike);
+    }
+    await this.usersLikesService.deleteByTmdbId(tmdb_id);
+    await this.moviesService.updateLikes(tmdb_id, 'SUBTRACT');
+    return res.status(HttpStatus.NO_CONTENT).send();
   }
 }
