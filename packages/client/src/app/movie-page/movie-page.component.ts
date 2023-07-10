13f0aa68ca08b4fe36b2faf264a5c06484bd4daf
Henrique Artur <contato@henriqueartur.com>
2023-07-10 18:44:28 -0300

    :sparkles: Change header for nav

--- packages/client/src/app/movie-page/movie-page.component.ts
+++ packages/client/src/app/movie-page/movie-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Movie } from '../movie/movie.model';
-
-const TMDB_IMG_ROOT_PATH = 'https://image.tmdb.org/t/p/original';
+import { MoviesService } from '../_services/movies.services';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-movie-page',
@@ -9,20 +9,19 @@ const TMDB_IMG_ROOT_PATH = 'https://image.tmdb.org/t/p/original';
   styleUrls: ['./movie-page.component.sass'],
 })
 export class MoviePageComponent {
-  movies: Movie[] = [
-    {
-      position: 1,
-      imagePath: `${TMDB_IMG_ROOT_PATH}${'/4yycSPnchdNAZirGkmCYQwTd3cr.jpg'}`,
-      title: 'Guardiões da Galáxia: Vol. 3',
-      likes: 1,
-      isLiked: false,
-    },
-    {
-      position: 2,
-      imagePath: `${TMDB_IMG_ROOT_PATH}${'/wDWAA5QApz5L5BKfFaaj8HJCAQM.jpg'}`,
-      title: 'Guardiões da Galáxia: Vol. 3',
-      likes: 100,
-      isLiked: true,
-    },
-  ];
+  movies: Movie[] = [];
+
+  constructor(private moviesServices: MoviesService) {}
+
+  public ngOnInit(): void {
+    this.moviesServices
+      .trendMovies()
+      .pipe(take(1))
+      .subscribe({
+        next: (movies) => {
+          this.movies = movies;
+        },
+        error: (_) => {},
+      });
+  }
 }
